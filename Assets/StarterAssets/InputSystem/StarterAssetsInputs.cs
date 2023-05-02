1482e5f4bca62862952dbd403ba0a27c694892f2
Jesse <jessealas6@gmail.com>
2023-05-02 17:47:22 -0600

    Mejoras no funcionales

--- Assets/StarterAssets/InputSystem/StarterAssetsInputs.cs
+++ Assets/StarterAssets/InputSystem/StarterAssetsInputs.cs
@@ -8,40 +8,40 @@ namespace StarterAssets
 	public class StarterAssetsInputs : MonoBehaviour
 	{
 		[Header("Character Input Values")]
-		public Vector2 move;
-		public Vector2 look;
-		public bool jump;
-		public bool sprint;
+		[SerializeField] private Vector2 move;
+        [SerializeField] private Vector2 look;
+        [SerializeField] private bool jump;
+        [SerializeField] private bool sprint;
 
 		[Header("Movement Settings")]
-		public bool analogMovement;
+        [SerializeField] private bool analogMovement;
 
 		[Header("Mouse Cursor Settings")]
-		public bool cursorLocked = true;
-		public bool cursorInputForLook = true;
+        [SerializeField] private bool cursorLocked = true;
+        [SerializeField] private bool cursorInputForLook = true;
 
 #if ENABLE_INPUT_SYSTEM
-		public void OnMove(InputValue value)
+		public void OnMove(InputAction.CallbackContext value)
 		{
-			MoveInput(value.Get<Vector2>());
+			MoveInput(value.ReadValue<Vector2>());
 		}
 
-		public void OnLook(InputValue value)
+		public void OnLook(InputAction.CallbackContext value)
 		{
 			if(cursorInputForLook)
 			{
-				LookInput(value.Get<Vector2>());
+				LookInput(value.ReadValue<Vector2>());
 			}
 		}
 
-		public void OnJump(InputValue value)
+		public void OnJump(InputAction.CallbackContext value)
 		{
-			JumpInput(value.isPressed);
+			JumpInput(value.action.triggered);
 		}
 
-		public void OnSprint(InputValue value)
+		public void OnSprint(InputAction.CallbackContext value)
 		{
-			SprintInput(value.isPressed);
+			SprintInput(value.action.ReadValue<float>() == 1);
 		}
 #endif
 
@@ -64,6 +64,27 @@ namespace StarterAssets
 		public void SprintInput(bool newSprintState)
 		{
 			sprint = newSprintState;
+		}
+		public Vector2 GetLock()
+		{
+			return look;
+		}
+
+		public Vector2 GetMove()
+		{
+			return move;
+		}
+		public bool IsJumping()
+		{
+			return jump;
+		}
+		public bool IsSprinting()
+		{
+			return sprint;
+		}
+		public bool IsAnalog()
+		{
+			return analogMovement;
 		}
 		
 		private void OnApplicationFocus(bool hasFocus)
